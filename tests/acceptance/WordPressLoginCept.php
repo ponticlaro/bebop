0928ce76339fbe6fc2ecc8f135180fcd5d09f1e3
Cristiano Baptista <cristianobaptista@designdesu.com>
2014-05-09 16:19:38 +0100

    changed input fields selectors for login test

--- tests/acceptance/WordPressLoginCept.php
+++ tests/acceptance/WordPressLoginCept.php
@@ -3,7 +3,7 @@
 $I = new WebGuy($scenario);
 $I->wantTo('Sign into WordPress');
 $I->amOnPage('/wp-login.php');
-$I->fillField('#user_login', 'admin');
-$I->fillField('#user_pass','123456789');
+$I->fillField('form input[type="text"]', 'admin');
+$I->fillField('form input[type="password"]','123456789');
 $I->click('#wp-submit');
 $I->seeCurrentUrlEquals('/wp-admin/');
