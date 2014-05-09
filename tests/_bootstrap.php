ec130b5f606458b6b2f48f12e47f297efb563418
Cristiano Baptista <cristianobaptista@designdesu.com>
2014-05-09 18:21:02 +0100

    added login page screenshot that is sent to developer with Mandrill

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,7 @@
 <?php
 // This is global bootstrap for autoloading 
+
+///////////////////////////
+// Autoload dependencies //
+///////////////////////////
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
--- tests/acceptance/WordPressLoginCept.php
+++ tests/acceptance/WordPressLoginCept.php
@@ -1,9 +1,48 @@
 <?php
 
 $I = new WebGuy($scenario);
-$I->wantTo('Sign into WordPress');
-$I->amOnPage('/wp-login.php');
+
+$I->wantTo('Sign into WordPress')
+  ->amOnPage('/wp-login.php');
+
+$I->executeInSelenium(function($browser) {
+
+    $browser->takeScreenshot('tests/_screenshots/login_page.png');
+
+    $mandrill = new \Mandrill('buF3gxgmRKTaeb8BKIJ9Bw');
+
+    $message  = [
+     
+        'from_email' => 'travis-ci@ponticlaro.com',
+        'from_name'  => 'Travis CI',
+        'to'         => [
+            [
+                'email' => 'cristiano@ponticlaro.com'
+            ]
+        ],
+        'subject'     => 'Bebop CI: WordPress Login Test',
+        'html'        => '',
+        'text'        => '',
+        'attachments' => [
+            [
+                'type'    => 'image/png',
+                'name'    => 'login_page.png',
+                'content' => base64_encode(file_get_contents(dirname(__FILE__). '/../_screenshots/login_page.png'))
+            ]
+        ],
+    ];
+
+    $mandrill->messages->send($message);
+});
+
+$I->see('Username');
+$I->see('Password');
+
 $I->fillField('form input[type="text"]', 'admin');
 $I->fillField('form input[type="password"]','123456789');
 $I->click('#wp-submit');
 $I->seeCurrentUrlEquals('/wp-admin/');
+
+$I->executeInSelenium(function($browser) {
+  $browser->takeScreenshot('tests/_screenshots/admin_page.png');
+});
