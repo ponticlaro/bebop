485250af78967c4d3729a2b1051409b01060a756
Cristiano Baptista <cristianobaptista@designdesu.com>
2014-08-01 16:42:17 +0100

    refactored/cleaned AdminPage class

--- src/Ponticlaro/Bebop/AdminPage.php
+++ src/Ponticlaro/Bebop/AdminPage.php
@@ -11,171 +11,252 @@ class AdminPage
 	 * 
 	 * @var Ponticlaro\Bebop\Common\Collection
 	 */
-	protected $__config;
+	protected $config;
 
 	/**
 	 * See http://codex.wordpress.org/Administration_Menus#Page_Hook_Suffix
 	 * 
 	 * @var string
 	 */
-	protected $__hook_suffix;
+	protected $hook_suffix;
 
 	/**
-	 * Instantiates a new Admin 
+	 * Instantiates a new Admin page
 	 * 
-	 * @param string   $title  Admin page tile. Used to create its access slug
-	 * @param callable $fn     Function to call that will contain all the page logic
-	 * @param array    $config Administration Menus API configuration
+	 * @param string   $title    Admin page tile. Used to create its access slug
+	 * @param callable $function Function to call that will contain all the page logic
 	 */
-	public function __construct($title, $fn, array $config = array())
+	public function __construct($title, $function)
 	{
-		// Take any necessary actions to make this object usable
-		$this->__preInit();
+		// Set config object with default configuration
+		$this->config = Bebop::Collection(array(
+			'page_title' => '',
+			'menu_title' => '',
+			'capability' => 'manage_options',
+			'menu_slug'  => '',
+			'function'   => '',
+			'icon_url'   => '',
+			'position'   => null,
+			'parent'     => ''
+		));
 
-		// Initialize class
-		call_user_func_array(array($this, '__init'), func_get_args());
+		// Set Page Title
+		if ($title)
+			$this->setPageTitle($title);
+
+		// Set Function
+		if ($function)
+			$this->setFunction($function);
+
+		// Register Admin Page
+		add_action('admin_menu', array($this, 'handleRegistration'));
 	}
 
-	private function __preInit()
+	/**
+	 * Sets page title
+	 * 
+	 * @param string $title
+	 */
+	public function setPageTitle($title)
 	{
-		$default_config = array(
-			'page_title'    => '',
-			'menu_title'    => '',
-			'capability'    => 'read',
-			'menu_slug'     => '',
-			'function'      => '',
-			'icon_url'      => '',
-			'position'      => null,
-			'parent'        => '',
-			'auto_register' => true
-		);
+		if (!is_string($title))
+			throw new \Exception('AdminPage title must be a string');
 
-		$this->__config = Bebop::Collection($default_config);
-	}
+		$this->config->set('page_title', $title);
 
-	private function __init()
-	{
-		call_user_func_array(array($this, '__handleInit'), func_get_args());
+		if (!$this->config->get('menu_title'))
+			$this->config->set('menu_title', $title);
+
+		if (!$this->config->get('menu_slug'))
+			$this->config->set('menu_slug', str_replace(" ", "-", strtolower($title)));
 
 		return $this;
 	}
 
-	private function __handleInit($title, $fn, array $config = array())
+	/**
+	 * Returns page title
+	 * 
+	 * @return string
+	 */
+	public function getPageTitle()
 	{
-		if( isset($title) ){
+		return $this->config->get('page_title');
+	}
 
-			if( is_array($title) ) {
+	/**
+	 * Sets menu title
+	 * 
+	 * @param string $title
+	 */
+	public function setMenuTitle($title)
+	{
+		if (!is_string($title))
+			throw new \Exception('AdminPage menu title must be a string');
 
-				$this->__config->set($title);
+		$this->config->set('menu_title', $title);
 
-			} elseif ( is_string( $title )) {
+		return $this;
+	}
 
-				$this->__config->set('page_title', $title);
+	/**
+	 * Returns menu title
+	 * 
+	 * @return string
+	 */
+	public function getMenuTitle()
+	{
+		return $this->config->get('menu_title');
+	}
 
-			} else {
+	/**
+	 * Sets menu slug
+	 * 
+	 * @param string $slug
+	 */
+	public function setMenuSlug($slug)
+	{
+		if (!is_string($slug))
+			throw new \Exception('AdminPage menu slug must be a string');
 
-				throw new \ErrorException('First argument must be an associative array or string');
+		$this->config->set('menu_slug', $slug);
 
-			}
+		return $this;
+	}
 
-		} 
+	/**
+	 * Returns menu slug
+	 * 
+	 * @return string
+	 */
+	public function getMenuSlug()
+	{
+		return $this->config->get('menu_slug');
+	}
 
-		if( isset($fn) ) {
+	/**
+	 * Sets parent page
+	 * 
+	 * @param string $parent
+	 */
+	public function setParent($parent)
+	{
+		if (!is_string($parent))
+			throw new \Exception('AdminPage parent must be a string');
+
+		$this->config->set('parent', $parent);
+
+		return $this;
+	}
+
+	/**
+	 * Returns parent
+	 * 
+	 * @return string
+	 */
+	public function getParent()
+	{
+		return $this->config->get('parent');
+	}
+
+	/**
+	 * Sets capability
+	 * 
+	 * @param string $capability
+	 */
+	public function setCapability($capability)
+	{
+		if (!is_string($capability))
+			throw new \Exception('AdminPage capability must be a string');
+
+		$this->config->set('capability', $capability);
+
+		return $this;
+	}
+
+	/**
+	 * Returns capability
+	 * 
+	 * @return string
+	 */
+	public function getCapability()
+	{
+		return $this->config->get('capability');
+	}
+
+	/**
+	 * Sets function
+	 * 
+	 * @param callable $fn
+	 */
+	public function setFunction($function)
+	{	
+		if (!is_callable($function))
+			throw new \Exception('AdminPage function must be callable');
 			
-			if( is_string($fn) & !is_callable($fn) ){
+		$this->config->set('function', $function);
 
-				throw new \ErrorException('Second argument must be callable');
-
-			}
-
-			$this->__config->set('function', $fn);
-
-		}
-
-		// Set 
-		if( isset($config) ) {
-
-			if( !is_array($config) ) throw new \ErrorException('Third argument must be a config array');
-
-			$this->__config->set($config);
-
-		}
-
-		// Set defaults, validate and fix config
-		$this->__validateConfig();
-
-		// Auto register admin page
-		if( $this->__config->get('auto_register') ) {
-
-			$this->register();
-
-		}
-
-	}
-
-	private function __validateConfig()
-	{
-		$page_title = $this->__config->get('page_title');
-		$menu_title = $this->__config->get('menu_title');
-		$function   = $this->__config->get('function');
-
-		// Throw error is neither page_title or menu_title was defined
-		if( !$page_title && ! $menu_title ){
-			throw new \ErrorException('You must set either the page_title or menu_title parameter');
-		}
-
-		// Throw error is no function was defined 
-		if( !$function ){
-			throw new \ErrorException("You must set the 'function' parameter");
-		}
-
-		// Auto fill page_title from menu_title
-		if( !$this->__config->get('page_title') ) {
-			$this->__config->set('page_title', $menu_title);
-		}
-
-		// Auto fill menu_title from page_title
-		if( !$this->__config->get('menu_title') ) {
-			$this->__config->set('menu_title', $page_title);
-		}
-
-		// Auto generate menu-_slug from page_title or menu_title
-		if( !$this->__config->get('menu_slug') ) {
-			$title = isset($page_title) ? $page_title : $menu_title; 
-			$this->__config->set('menu_slug', str_replace(" ", "-", strtolower($title) ) );
-		}
-
-		// Set capability to manage_options by default
-		if( !$this->__config->get('capability') ) {
-			$this->__config->set('capability', 'manage_options');
-		}
-
-		// Set parent to null by default
-		if( !$this->__config->get('position') ) {
-			$this->__config->set('position', null);
-		}
-
-		// Set auto_register to true by default
-		if( !$this->__config->get('auto_register') ) {
-			$this->__config->set('auto_register', true);
-		}
-		
-	}
-
-	/**
-	 * Registers this page with the add_action function on the "admin_menu" hook
-	 * 
-	 * @return void
-	 */
-	public function register()
-	{
-		if( $this->__config->get('function') ){
-			add_action( 'admin_menu', array( $this, 'handleRegistration' ) );
-		}
-		
 		return $this;
 	}
 
+	/**
+	 * Returns function
+	 * 
+	 * @return callable
+	 */
+	public function getFunction()
+	{
+		return $this->config->get('function');
+	}
+
+	/**
+	 * Sets position
+	 * 
+	 * @param mixed $position
+	 */
+	public function setPosition($position)
+	{	
+		if (!is_string($position) && !is_integer($position))
+			throw new \Exception('AdminPage position must be either a string or an integer');
+			
+		$this->config->set('position', $position);
+
+		return $this;
+	}
+
+	/**
+	 * Returns position
+	 * 
+	 * @return mixed
+	 */
+	public function getPosition()
+	{
+		return $this->config->get('position');
+	}
+
+	/**
+	 * Sets icon url
+	 * 
+	 * @param mixed $url
+	 */
+	public function setIconUrl($url)
+	{	
+		if (!is_string($url))
+			throw new \Exception('AdminPage icon url must be a string');
+			
+		$this->config->set('icon_url', $url);
+
+		return $this;
+	}
+
+	/**
+	 * Returns icon url
+	 * 
+	 * @return string
+	 */
+	public function getIconUrl()
+	{
+		return $this->config->get('icon_url');
+	}
 
 	/**
 	 * Defines which function should be used based on the settings provided
@@ -185,11 +266,11 @@ class AdminPage
 	 */
 	public function handleRegistration()
 	{
-		$parent = $this->__config->get('parent');
+		$parent = $this->getParent();
 
-		if( $parent ){
+		if ($parent) {
 
-			switch ( $parent ) {
+			switch ($parent) {
 				case 'dashboard':
 					$fn = 'add_dashboard_page';
 					break;
@@ -239,55 +320,63 @@ class AdminPage
 					break;
 			}
 
-			if($fn == 'add_submenu_page'){
+			if ($fn == 'add_submenu_page') {
 
-				$hook_suffix = $fn(
-					$this->__config->get('parent'),
-					$this->__config->get('page_title'), 
-					$this->__config->get('menu_title'), 
-					$this->__config->get('capability'), 
-					$this->__config->get('menu_slug'), 
+				$fn(
+					$this->getParent(),
+					$this->getPage_title(), 
+					$this->getMenu_title(), 
+					$this->getCapability(), 
+					$this->getMenuSlug(), 
 					array($this, 'baseHtml')
 				);
-
-			}else{
-
-				$hook_suffix = $fn(
-					$this->__config->get('page_title'), 
-					$this->__config->get('menu_title'), 
-					$this->__config->get('capability'), 
-					$this->__config->get('menu_slug'), 
-					array($this, 'baseHtml')
-				);
-
 			}
 
+			else {
 
-		}else{
+				$fn(
+					$this->getPageTitle(), 
+					$this->getMenuTitle(), 
+					$this->getCapability(), 
+					$this->getMenuSlug(), 
+					array($this, 'baseHtml')
+				);
+			}
+		}
 
-			$hook_suffix = add_menu_page(
-				$this->__config->get('page_title'), 
-				$this->__config->get('menu_title'), 
-				$this->__config->get('capability'), 
-				$this->__config->get('menu_slug'), 
+		else {
+
+			add_menu_page(
+				$this->getPageTitle(), 
+				$this->getMenuTitle(), 
+				$this->getCapability(), 
+				$this->getMenuSlug(), 
 				array($this, 'baseHtml'), 
-				$this->__config->get('icon_url'), 
-				$this->__config->get('position') 
+				$this->getIconUrl(), 
+				$this->getPosition() 
 			);
-
-		}		
-
-		//$this->__hook_suffix = $hook_suffix;
+		}	
 	}
 
 	/**
-	 * Still not working
-	 * See http://codex.wordpress.org/Administration_Menus#Page_Hook_Suffix
+	 * Removes this admin page
 	 * 
-	 * @return string Returns the hook in which we can attach actions to, when this page is loaded
+	 * @return void
 	 */
-	public function getHook(){
-		//return 'load-'. $this->__config->get(hook_suffix;
+	public function destroy()
+	{
+		remove_menu_page($this->getMenuSlug());
+	}
+
+	/**
+	 * Checks if the user have permission to access this page
+	 * 
+	 * @return void
+	 */
+	protected function __checkPermissions()
+	{
+		if (!current_user_can($this->getCapability()))
+			wp_die(__( 'You do not have sufficient permissions to access this page.'));
 	}
 
 	/**
@@ -298,45 +387,17 @@ class AdminPage
 	 */
 	public function baseHtml(){ 
 
-		$this->checkPermissions(); ?>
+		$this->__checkPermissions(); ?>
 
 		<div class="wrap">
 
 			<div id="icon-plugins" class="icon32"></div>
-			<h2><?php echo $this->__config->get('page_title'); ?></h2>
+			<h2><?php echo $this->getPageTitle(); ?></h2>
 			
 			<?php settings_errors(); ?>
-			
-			<?php
-				$fn = $this->__config->get('function');
-				call_user_func($fn);
-			?>
+			<?php call_user_func_array($this->getFunction(), array()); ?>
 			
 		</div><!-- /.wrap -->
 		
 	<?php }
-
-
-	/**
-	 * Checks if the user have permission to access this page
-	 * 
-	 * @return void
-	 */
-	protected function checkPermissions()
-	{
-		if ( !current_user_can( $this->__config->get('capability') ) )  {
-			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-		} 
-	}
-
-	/**
-	 * Removes this admin page
-	 * 
-	 * @return void
-	 */
-	public function destroy()
-	{
-		remove_menu_page( $this->__config->get('menu_slug') );
-	}
-
 }
