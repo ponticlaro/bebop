7c74389d68815fe18293b48e6020a306622a2a76
Cristiano Baptista <cristianobaptista@designdesu.com>
2014-08-06 15:35:45 +0100

    abstracted factory pattern

--- src/Ponticlaro/Bebop/Helpers/BebopFactory.php
+++ src/Ponticlaro/Bebop/Helpers/BebopFactory.php
@@ -3,10 +3,10 @@
 namespace Ponticlaro\Bebop\Helpers;
 
 use Ponticlaro\Bebop;
-use Ponticlaro\Bebop\Patterns\SingletonAbstract;
+use Ponticlaro\Bebop\Patterns\FactoryAbstract;
 use Ponticlaro\Bebop\Patterns\TrackableObjectAbstract;
 
-class BebopFactory {
+class BebopFactory extends FactoryAbstract {
 
 	/**
 	 * List of manufacturable classes
@@ -23,80 +23,20 @@ class BebopFactory {
 	);
 
 	/**
-	 * Making sure class cannot get instantiated
-	 */
-	protected function __construct() {}
-
-	/**
-	 * Making sure class cannot get instantiated
-	 */
-	protected function __clone() {}
-
-	/**
-	 * Adds a new manufacturable class
-	 * 
-	 * @param string $type  Object type ID
-	 * @param string $class Full namespace for a class
-	 */
-	public static function set($type, $class)
-	{
-		self::$manufacturable[$type] = $class;
-	}
-
-	/**
-	 * Removes a new manufacturable class
-	 * 
-	 * @param string $type  Object type ID
-	 */
-	public static function remove($type)
-	{
-		if (isset(self::$manufacturable[$type])) unset(self::$manufacturable[$type]);
-	}
-
-	/**
 	 * Creates instance of target class
 	 * 
-	 * @param  string] $type Class ID
-	 * @param  array   $args Class arguments
-	 * @return object        Class instance
+	 * @param  string $type Class ID
+	 * @param  array  $args Class arguments
+	 * @return object       Class instance
 	 */
 	public static function create($type, array $args = array())
 	{
-		// Check if target is in the allowed list
-		if (array_key_exists($type, self::$manufacturable)) {
+		// Create object
+		$obj = parent::create($type, $args);
 
-			$class_name = self::$manufacturable[$type];
+		// Track object if trackable
+	    if ($obj instanceof TrackableObjectAbstract) Bebop::track($obj);
 
-			return call_user_func(array(__CLASS__, "__createInstance"), $class_name, $args);
-		}
-
-		// Return null if target object is not manufacturable
-		return null;
-	}
-
-	/**
-	 * Creates and instance of the target class
-	 * 
-	 * @param  string $class_name Target class
-	 * @param  array  $args       Arguments to pass to target class
-	 * @return mixed              Class instance or false
-	 */
-	private static function __createInstance($class_name, array $args = array())
-	{
-    	// Get an instance of the target class
-        $obj = call_user_func_array(
-
-        	array(
-				new \ReflectionClass($class_name), 
-				'newInstance'
-			), 
-            $args
-        );
-        	
-    	// Track object if trackable
-    	if ($obj instanceof TrackableObjectAbstract) Bebop::track($obj);
-
-    	// Return object
-    	return $obj;
+	    return $obj;
 	}
 }
